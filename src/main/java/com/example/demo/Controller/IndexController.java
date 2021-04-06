eae5cebe600e068256784374f9bc4a7e5b3b0f61
Q <feifeiandy@outlook.com>
2021-04-06 17:28:59 +0800

    热部署

--- src/main/java/com/example/demo/Controller/IndexController.java
+++ src/main/java/com/example/demo/Controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/Index")
+    @RequestMapping("/")
     public String Index()
     {
         return "Index";
